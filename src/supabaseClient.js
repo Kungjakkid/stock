cac74df2e94a959010bbb8a9941697e532f18b24
Kungjakkid <148857819+Kungjakkid@users.noreply.github.com>
2026-05-05 11:42:26 +0700

    Hardcode API for easy deployment

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+// Hardcoded for easier deployment to Vercel
+const supabaseUrl = 'https://pahfjtmzytcokxmlblea.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBhaGZqdG16eXRjb2t4bWxibGVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3NTQ5NTEsImV4cCI6MjA5MzMzMDk1MX0.dDhTJq4Y7jeMx8UXu8yB07xQE2M1pFC1eLzZW53aBis';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
